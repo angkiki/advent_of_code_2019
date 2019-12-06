3afa61b39db0737fdec67de206738b514483327c
angkiki <gabrielangzw@gmail.com>
2019-12-06 14:42:29 +0800

    add orbit methods

--- day_6/_orbit.ts
+++ day_6/_orbit.ts
@@ -1,13 +1,24 @@
 export class Orbit {
   parent: Orbit | null;
+  children: Orbit[];
   directOrbits: number;
   indirectOrbits: number;
 
   constructor(parent: Orbit | null) {
     this.parent = parent;
+    this.children = [];
     this.directOrbits = parent ? 1 : 0;
     this.indirectOrbits = parent
       ? parent.directOrbits + parent.indirectOrbits
       : 0;
+  }
+
+  addParent(parent: Orbit) {
+    this.directOrbits = 1;
+    this.indirectOrbits = parent.directOrbits + parent.indirectOrbits;
+  }
+
+  addChild(child: Orbit) {
+    this.children.push(child);
   }
 }
