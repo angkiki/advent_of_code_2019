5540fff4b4d2fd3bb1c2df2aba4438c661ee4192
angkiki <gabrielangzw@gmail.com>
2019-12-06 15:29:44 +0800

    improve orbit class

--- day_6/_orbit.ts
+++ day_6/_orbit.ts
@@ -11,16 +11,6 @@ export class Orbit {
     this.indirectOrbits = 0;
   }
 
-  addParent(parent: Orbit) {
-    if (this.parent !== null) {
-      throw Error("This orbit already has a parent");
-    }
-
-    this.parent = parent;
-    this.directOrbits = 1;
-    this.indirectOrbits = parent.directOrbits + parent.indirectOrbits;
-  }
-
   addChild(child: Orbit) {
     this.children.push(child);
     this.reconcileChildOrbits(child);
@@ -31,18 +21,11 @@ export class Orbit {
     child.directOrbits = 1;
     child.indirectOrbits = this.directOrbits + this.indirectOrbits;
 
-    child.children.forEach(c => this.reconcileChildOrbits(c));
+    child.children.forEach(c => child.reconcileChildOrbits(c));
   }
 }
 
-export const findTotalOrbits = (orbit: Orbit): number => {
-  if (orbit.children.length === 0) {
-    return orbit.directOrbits + orbit.indirectOrbits;
-  }
-
-  return (
-    orbit.directOrbits +
-    orbit.indirectOrbits +
-    orbit.children.reduce((acc, curr) => acc + findTotalOrbits(curr), 0)
-  );
-};
+export const findTotalOrbits = (orbit: Orbit): number =>
+  orbit.directOrbits +
+  orbit.indirectOrbits +
+  orbit.children.reduce((acc, curr) => acc + findTotalOrbits(curr), 0);
