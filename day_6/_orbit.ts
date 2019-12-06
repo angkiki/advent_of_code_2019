41e18ec17febcaab2a62e1055e621f6f5676930d
angkiki <gabrielangzw@gmail.com>
2019-12-06 16:55:29 +0800

    update orbit

--- day_6/_orbit.ts
+++ day_6/_orbit.ts
@@ -1,10 +1,12 @@
 export class Orbit {
+  name: string;
   parent: Orbit | null;
   children: Orbit[];
   directOrbits: number;
   indirectOrbits: number;
 
-  constructor() {
+  constructor(name: string) {
+    this.name = name;
     this.parent = null;
     this.children = [];
     this.directOrbits = 0;
@@ -29,3 +31,37 @@ export const findTotalOrbits = (orbit: Orbit): number =>
   orbit.directOrbits +
   orbit.indirectOrbits +
   orbit.children.reduce((acc, curr) => acc + findTotalOrbits(curr), 0);
+
+export const findCommonParent = (start: Orbit, target: Orbit): number => {
+  const startArray = [];
+  const targetArray = [];
+
+  let currStart = start;
+  let currTarget = target;
+
+  while (true) {
+    currStart = currStart.parent ? currStart.parent : currStart;
+    currTarget = currTarget.parent ? currTarget.parent : currTarget;
+
+    targetArray.push(currTarget.name);
+    startArray.push(currStart.name);
+
+    if (startArray.includes(currTarget.name)) {
+      let index = startArray.indexOf(currTarget.name);
+      startArray.splice(index + 1);
+      break;
+    }
+
+    if (targetArray.includes(currStart.name)) {
+      let index = targetArray.indexOf(currStart.name);
+      targetArray.splice(index + 1);
+      break;
+    }
+
+    if (!currStart.parent && !currTarget.parent) {
+      break;
+    }
+  }
+
+  return startArray.length + targetArray.length;
+};
