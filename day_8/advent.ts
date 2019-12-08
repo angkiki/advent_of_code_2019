fd14aa79db52fa116bbaa48fbef1f6befc4e0d77
angkiki <gabrielangzw@gmail.com>
2019-12-08 23:38:50 +0800

    finish day 8

--- day_8/advent.ts
+++ day_8/advent.ts
@@ -8,24 +8,53 @@ interface IResult {
 const WIDTH = 25;
 const HEIGHT = 6;
 
+const printLayer = (layer: number[]) => {
+  for (let i = 0; i < 6; i++) {
+    const unformattedLayer = layer.slice(i * 25, (i + 1) * 25);
+    const formattedLayer = unformattedLayer.map((num: number) =>
+      num === 0 ? "." : "#"
+    );
+    console.log(formattedLayer.join(""));
+  }
+};
+
+const constructLayer = (
+  currentLayer: number[],
+  nextLayer: number[]
+): number[] => {
+  currentLayer.forEach((pixel: number, index: number) => {
+    if (pixel === 2) {
+      currentLayer[index] = nextLayer[index];
+    }
+  });
+
+  return currentLayer;
+};
+
+const hasTransparentPixel = (layer: number[]): boolean => {
+  for (let i = 0; i < layer.length; i++) {
+    if (layer[i] === 2) return true;
+  }
+
+  return false;
+};
+
 fs.readFile("./data.txt", "utf-8", (err: Error, data: string) => {
   if (err) throw err;
 
   const dataArray = data.split("");
 
   const partOneCloneData = dataArray.map((n: string) => +n);
-  const partOneLayers = [];
-
   const partOneResult = { length: 0, index: 0 };
+  const layers = [];
 
   while (partOneCloneData.length > 0) {
     const temp = partOneCloneData.splice(0, WIDTH * HEIGHT);
-    partOneLayers.push(temp);
+    layers.push(temp);
   }
 
-  partOneLayers.forEach((layer: number[], index: number) => {
-    // console.log("layer -> ", layer.length);
-    const leastZeros = layer.filter((num: number) => num !== 0);
+  layers.forEach((layer: number[], index: number) => {
+    const leastZeros = layer.filter((num: number) => num > 0);
     if (leastZeros.length > partOneResult.length) {
       partOneResult.length = leastZeros.length;
       partOneResult.index = index;
@@ -34,20 +63,24 @@ fs.readFile("./data.txt", "utf-8", (err: Error, data: string) => {
 
   let numberOfOnes = 0;
   let numberOfTwos = 0;
-  console.log("part one result -> ", partOneResult);
-  const leastCorruptedImage = partOneLayers[partOneResult.index];
-  console.log("least corruptedImage");
-  for (let i = 0; i < 6; i++) {
-    let result = leastCorruptedImage.slice(25 * i, 25 * (i + 1));
-    console.log(result.join(""));
-  }
-  console.log("####");
+  const leastCorruptedImage = layers[partOneResult.index];
   leastCorruptedImage.forEach((n: number) => {
     if (n === 1) numberOfOnes += 1;
-    if (n === 2) numberOfTwos += 2;
+    if (n === 2) numberOfTwos += 1;
   });
 
+  console.log("computing part one....");
   console.log("number of ones ->", numberOfOnes);
   console.log("number of twos ->", numberOfTwos);
   console.log("ones * twos -> ", numberOfOnes * numberOfTwos);
+
+  console.log("computing part two....");
+  let finalImage = layers[0];
+  let nextImageIndex = 1;
+  while (hasTransparentPixel(finalImage)) {
+    finalImage = constructLayer(finalImage, layers[nextImageIndex]);
+    nextImageIndex += 1;
+  }
+
+  printLayer(finalImage);
 });
