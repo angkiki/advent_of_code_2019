7f103b16f8ea2afa5ed32b0d8ee7ad233d0526a6
angkiki <gabrielangzw@gmail.com>
2019-12-17 11:05:28 +0800

    still not efficient enough

--- day_16/_fft.ts
+++ day_16/_fft.ts
@@ -73,9 +73,6 @@ const applyEfficientPhase = (signal: number[], offset: number): number => {
   return total;
 };
 
-const n = [1, 2, 3, 4, 5, 6, 7, 8];
-applyEfficientPhase(n, 1);
-
 export const efficientSignalOffset = (signal: number[]): number[] => {
   const newSignal = [];
 
--- day_16/advent.ts
+++ day_16/advent.ts
@@ -15,4 +15,21 @@ fs.readFile('./data.txt', 'utf-8', (err: Error, data: string) => {
   }
   console.log('result is -> ', currInputArray.slice(0, 8).join(''));
   console.log('result is -> ', efficientInput.slice(0, 8).join(''));
+
+  console.log('deriving part two input ...');
+  let realInput = [];
+  for (let i = 0; i < 10000; i++) {
+    realInput.push(...inputArray);
+  }
+  console.log('done deriving real input ...');
+  console.log('computing part two output....');
+  for (let i = 0; i < 100; i++) {
+    realInput = efficientSignalOffset(realInput);
+    console.log('done with cycle ', i);
+  }
+  console.log('done computing part two ...');
+
+  const partTwoOffset = +inputArray.slice(0, 7).join('');
+  const realPartTwoAnswer = realInput.slice(partTwoOffset, partTwoOffset + 8);
+  console.log('part two answer is: ', realPartTwoAnswer.join(''));
 });
